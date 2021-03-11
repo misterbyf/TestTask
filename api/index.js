23a144f3e8cb028fcb9c8b67590e69dc6d70e23d
bogdan <bogdaniys10@gmail.com>
2021-03-12 00:47:54 +0200

    initial commit

---
+++ api/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+// eslint-disable-next-line no-unused-vars
+const dotenv = require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+async function runServer() {
+  try {
+    app.listen(PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Server has been started on port: ${PORT}`);
+    });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(`Server message: ${e.message}`);
+    process.exit(1);
+  }
+}
+
+runServer().catch(error => {
+  // eslint-disable-next-line no-console
+  console.log(error.message);
+});
