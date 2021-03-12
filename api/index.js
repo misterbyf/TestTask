e6cb7c5db179da3913223f78176e62f870ecab62
bogdan <bogdaniys10@gmail.com>
2021-03-12 21:40:15 +0200

    add controller and login rout

--- api/index.js
+++ api/index.js
@@ -1,29 +1,36 @@
 // eslint-disable-next-line strict
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 // eslint-disable-next-line no-unused-vars
 const dotEnv = require('dotenv').config();
+const authRouter = require('./routes/auth');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const URL = process.env.URL;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use('/api/auth', authRouter);
+
 async function runServer() {
   try {
+    await mongoose.connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
     app.listen(PORT, () => {
-      // eslint-disable-next-line no-console
       console.log(`Server has been started on port: ${PORT}`);
     });
   } catch (e) {
-    // eslint-disable-next-line no-console
     console.log(`Server message: ${e.message}`);
     process.exit(1);
   }
 }
 
 runServer().catch(error => {
-  // eslint-disable-next-line no-console
   console.log(error.message);
 });
